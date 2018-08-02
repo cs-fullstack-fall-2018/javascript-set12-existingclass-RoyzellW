5d2248c3121dd6d5370f9d0375327851fc17bee8
student <royzellwalton@gmail.com>
2018-08-02 11:34:46 -0500

    Done

--- Account.js
+++ Account.js
@@ -22,6 +22,24 @@ class Account{
     }
 }
 
+//my code is self-documenting (._.) lol
+//problem number 29 and 30 are together
 function main() {
 
+    var Matt = new Account("Matt", 1000);
+    var MyAccount = new Account("My Account", 0);
+    var Bob = new Account("Bob", 100);
+
+    Bob.withdrawal(20);
+
+    Matt.withdrawal(100);
+
+    MyAccount.deposit(100);
+
+    alert("Remaining Balance: " + Matt.balance());
+    alert("Remaining Balance: " + MyAccount.balance());
+
+
 }
+
+main();
--- Person.js
+++ Person.js
@@ -23,6 +23,27 @@ class Person {
 
 function main() {
 
+    var Bob = new Person("Bob", 17, 145, 160);
+    var Carl = new Person("Carl", 21, 150, 190);
+
+    var bobArray = [Bob, Carl];
+
+
+    // Bob.name = "Bob";
+    // Bob.age = 17;
+    // Bob.weight = 145;
+    // Bob.height = 160;
+    //
+    // Person.Carl = new Person();
+    // Carl.name = "Carl";
+    // Carl.age = 17;
+    // Carl.weight = 145;
+    // Carl.height = 160;
+
+alert(Bob.name + " has a body mass index of: " + Bob.bodyMassIndex());
+alert(Carl.name + " has a body mass index of: " + Carl.bodyMassIndex());
+bobArray[0].printPerson();
+
 }
 
 main();
